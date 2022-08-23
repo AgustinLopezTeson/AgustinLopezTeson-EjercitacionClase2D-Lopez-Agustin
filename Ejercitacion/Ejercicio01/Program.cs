cae81b6ecd4a832dbab5f19bc34c0facca6d6d66
AgustinLopezTeson <agustinlopezteson@gmmail.com>
2022-08-23 00:31:37 -0300

    Entry

--- Ejercitacion/Ejercicio01/Program.cs
+++ Ejercitacion/Ejercicio01/Program.cs
@@ -6,7 +6,35 @@ namespace Ejercicio01
     {
         static void Main(string[] args)
         {
+            int maximoNumeroIngresado=0;
+            int minimoNumeroIngresado = 0;
+            int promedioDeNumeroIngresados =0;
+            int numeroIngresadoInt;
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("Ingrese un numero ({0}): ", j+1);
+                    string numeroIngresado = Console.ReadLine();
+                    bool resultado = int.TryParse(numeroIngresado,out numeroIngresadoInt);
+
+                    while (resultado==false)
+                    {
+                        Console.WriteLine("Error ingrese un valor correcto: ");
+                        numeroIngresado = Console.ReadLine();
+                        resultado = int.TryParse(numeroIngresado, out numeroIngresadoInt);
+                    }
+
+                    minimoNumeroIngresado = numeroIngresadoInt;
+                    maximoNumeroIngresado = Math.Max(maximoNumeroIngresado, numeroIngresadoInt);
+                    minimoNumeroIngresado = Math.Min(minimoNumeroIngresado, numeroIngresadoInt);
+
+                    promedioDeNumeroIngresados += numeroIngresadoInt;
+                }
+                promedioDeNumeroIngresados /= 5;
             
+            Console.WriteLine("Maximo: {0}  | Minimo: {1}  | Promedio: {2}",maximoNumeroIngresado,minimoNumeroIngresado,promedioDeNumeroIngresados);
+
+
         }
     }
 }
+
---
+++ Ejercitacion/Ejercicio02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Ingrese un numero: ");
+            string numeros = Console.ReadLine();
+            bool resultado=int.TryParse(numeros, out numero);
+
+             while (resultado == false || numero<0)
+             {       
+                 Console.WriteLine("Error, Reingrese el numero");
+                 numeros = Console.ReadLine();
+                 resultado = int.TryParse(numeros, out numero);
+             } 
+
+            double cubo=Math.Pow(numero,3);
+            double cuadrado=Math.Pow(numero,2);
+
+            Console.WriteLine("Cubo de {0} es: {1}",numero,cubo);
+            Console.WriteLine("Cuadrado de {0} es: {1}",numero,cuadrado);
+
+        }
+    }
+}
