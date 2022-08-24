953c9ff448dc4170c1bd135d34b3e8e20228652f
AgustinLopezTeson <agustinlopezteson@gmmail.com>
2022-08-23 21:16:42 -0300

    Push

--- Ejercitacion/Ejercicio03/Program.cs
+++ Ejercitacion/Ejercicio03/Program.cs
@@ -6,7 +6,42 @@ namespace Ejercicio03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Primo");
+            Console.WriteLine("Ingrese un numero: ");
+            Console.WriteLine("Para terminar ingrese SALIR");
+
+            string opcionIngresada = Console.ReadLine();
+
+            if (opcionIngresada == "salir")
+                {
+                    Environment.Exit(0);
+                }
+
+            bool respuesta = int.TryParse(opcionIngresada, out int numeroInt);
+
+
+            while (respuesta == false)
+               {
+                   Console.WriteLine("Error,ingrese un dato valido");
+                   Console.WriteLine("Para terminar ingrese SALIR");
+
+                   if(opcionIngresada=="salir")
+                   {
+                       Environment.Exit(0);
+                   }
+               }
+
+            for (int i = 1; i <= numeroInt; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if(i%j==0 || i==2)
+                    {
+                        Console.WriteLine("El numero {0} es primo",i);
+                        break;
+                    }
+                }
+            }
+
         }
     }
 }
